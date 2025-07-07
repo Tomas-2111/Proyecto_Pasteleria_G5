3be770b5648a23444379046ca7970e32f8ec35bd
Tomas-2111 <tojoalgu@gmail.com>
2025-07-07 15:05:44 -0600

    Initial commit

---
+++ src/main/java/com/proyectoG5/ProyectoG5Application.java
@@ -0,0 +1,13 @@
+package com.proyectoG5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoG5/ProyectoG5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoG5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoG5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
