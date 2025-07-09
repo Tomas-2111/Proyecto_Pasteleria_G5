698943be35dd9bf6337a944cd035df4903b34497
erick.zuniga24 <ezuniga70056@ufide.ac.cr>
2025-07-09 02:08:38 -0600

    v.2
    Se incluye la pagina index y la pagina Menu

---
+++ src/main/java/com/proyectoG5/controller/PaginaController.java
@@ -0,0 +1,13 @@
+package com.proyectoG5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginaController {
+
+    @GetMapping("/menu")
+    public String mostrarMenu() {
+        return "menu"; // Se refiere a menu.html en /resources/templates/
+    }
+}
